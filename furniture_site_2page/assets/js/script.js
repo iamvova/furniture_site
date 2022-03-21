8a5468ef2b8aaf447f7b8abf2052dca73f3f30ff
Liknox <92331274+Liknox@users.noreply.github.com>
2022-03-21 18:25:53 +0200

    Add files via upload
    Finally finished 2 page of our project...

---
+++ furniture_site_2page/assets/js/script.js
@@ -0,0 +1,17 @@
+let lox = document.querySelector(".burger__top")
+
+function lohClick(){
+	alert("---------> Loh <--------- \nCHECK CONSOLE")
+	let a = "Ty vseodno loh"
+	let b = "______________"
+	num = 99999999999999999
+	for (var i = 0; i <= num; i++) {
+		i++
+		console.log(a)
+		console.log(b)
+	}
+	console.log(a)
+}
+
+
+lox.addEventListener('click', lohClick)
